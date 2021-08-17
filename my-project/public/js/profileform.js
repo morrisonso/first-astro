feaa887fa4d6df5f43fb8da729003a91876faf6c
charmainpdrgl <charmainpedrigal@gmail.com>
2021-08-17 16:04:48 +1200

    created a profile log form that saves user input and displays saved details including the time it had been saved

--- my-project/public/js/profileform.js
+++
@@ -1,9 +0,0 @@
-function results()
-{
-    var firstname = document.getElementById('firstname').value;
-    // var lastname = document.getElementById('lastname').value;
-    // var bio = document.getElementById('bio').value;
-    display.innerHTML = firstname;
-
-//document.getElementById("display").innerHTML = document.getElementById("firstname").value
-}
